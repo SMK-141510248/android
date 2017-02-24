cce6c76031ec80d969c990e3e7b6964269f2be3a
SMK-141510248 <141510248@smkassalaambandung.sch.id>
2017-02-24 20:32:34 +0700

    Android

---
+++ app/src/main/java/assalaam/studio/ujikom/app/AppConfig.java
@@ -0,0 +1,9 @@
+package assalaam.studio.ujikom.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://127.0.0.1:8000/Hari-pertama-ujikom-perbaikan-dirumah-master/public/api/login";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://ujikom-2017.smkassalaambandung.sch.id/api/register";
+}
